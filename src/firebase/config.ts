bfbf3fb12397b499476b8eb8336043b4bf205319
Yogendra Karri <yogendrakarri7@gmail.com>
2026-01-10 09:02:02 +0000

    fix it ## Error Type Console FirebaseError
    ## Error Message
    Firebase St

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,9 @@
 export const firebaseConfig = {
-  "projectId": "studio-6921718841-1defd",
-  "appId": "1:1031094079596:web:6c818f9b6e198403908849",
-  "apiKey": "AIzaSyA8r9HPMz2Y19XdiDPUc6hphdkFI0D1XqM",
-  "authDomain": "studio-6921718841-1defd.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "1031094079596"
+  apiKey: "AIzaSyAJMaFxCS4F8YPeDEdPtQLZFC5rqWdkpJs",
+  authDomain: "agrovision-ai-2683c.firebaseapp.com",
+  projectId: "agrovision-ai-2683c",
+  storageBucket: "agrovision-ai-2683c.appspot.com",
+  messagingSenderId: "680056027232",
+  appId: "1:680056027232:web:a1d85b6961fb4aee1bab05",
+  measurementId: "G-0VRZ7ZZVQ8"
 };
