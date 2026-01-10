1b6162c134c3a33c0f5a8af4da951355d6c6bfd4
Yogendra Karri <yogendrakarri7@gmail.com>
2026-01-10 09:08:02 +0000

    File changes

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
   "projectId": "studio-6921718841-1defd",
   "appId": "1:1031094079596:web:6c818f9b6e198403908849",
+  "storageBucket": "studio-6921718841-1defd.appspot.com",
   "apiKey": "AIzaSyA8r9HPMz2Y19XdiDPUc6hphdkFI0D1XqM",
   "authDomain": "studio-6921718841-1defd.firebaseapp.com",
-  "measurementId": "",
   "messagingSenderId": "1031094079596"
 };
