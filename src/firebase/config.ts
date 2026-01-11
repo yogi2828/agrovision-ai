ccc9d8c4ec7ca6510c63f4bdc8ba828bc516b8d3
sylago app <sylagoapp@gmail.com>
2026-01-11 12:39:17 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6519095482-d7f0a",
+  "appId": "1:680170897070:web:6ecf62ee3ecbe4fd5d773f",
+  "apiKey": "AIzaSyAxxCuPSG2kPHdBDTjw2Leh8AVPgMD4yxw",
+  "authDomain": "studio-6519095482-d7f0a.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "680170897070"
+};
