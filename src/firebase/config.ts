6243295dec9f9a521c6869f8ce10305f068ddde9
sylago app <sylagoapp@gmail.com>
2026-02-10 16:20:51 +0000

    Try fixing this error: `Console FirebaseError: Firebase: Error (auth/una

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -4,6 +4,5 @@ export const firebaseConfig = {
   "apiKey": "AIzaSyBoTlZ3lGNJtoD7izNX5gY46B1OIFXYScg",
   "authDomain": "agrovisionai-9af7d.firebaseapp.com",
   "measurementId": "G-CRRVJ6ZR9S",
-  "messagingSenderId": "266511304753",
-  "storageBucket": "agrovisionai-9af7d.firebasestorage.app"
+  "messagingSenderId": "266511304753"
 };
